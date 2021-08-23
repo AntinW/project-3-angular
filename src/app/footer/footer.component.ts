7e31905b4461cd52d660362fe1add1e05e522f34
Robin White <robintylerw@gmail.com>
2021-08-23 20:48:28 +0200

    updated

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,30 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   template:`
-    <footer class="footer">
-      <div class="container content has-text-centered is-dark">
-      
-      <a class="button is-medium is-facebook">
-        <span class="icon">
-          <i class="fab fa-facebook fa-lg"></i>
-        </span>
-      </a>
   
-      <a class="button is-medium is-instagram">
-        <span class="icon">
-          <i class="fab fa-instagram fa-lg"></i>
-        </span>
-      </a>
-      
+  <footer class="footer2">
+  <div class="container content has-text-centered ">
+   <a class="button is-medium is-facebook">
+    <span class="icon">
+      <i class="fab fa-facebook fa-lg"></i>
+    </span>
+  </a>
 
-      <a class="button is-medium is-twitter">
-        <span class="icon">
-          <i class="fab fa-twitter fa-lg"></i>
-        </span>
-      </a>
+<a class="button is-medium is-instagram">
+  <span class="icon">
+    <i class="fab fa-instagram fa-lg"></i>
+  </span>
+</a>
 
-      </div>
-      </footer>
+
+<a class="button is-medium is-twitter">
+  <span class="icon">
+    <i class="fab fa-twitter fa-lg"></i>
+  </span>
+</a>
+
+ 
+  </div>
+  </footer>
   
   `,
   styleUrls: ['./footer.component.css']
