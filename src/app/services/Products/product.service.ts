2d1f2c61eac31cc7d6826a8c5180b9bff6f5be9e
Robin <robintylerw@gmail.com>
2021-09-27 13:19:15 +0200

    Images displaying from database

--- src/app/services/Products/product.service.ts
+++ src/app/services/Products/product.service.ts
@@ -1,47 +1,17 @@
 import { Injectable,  } from '@angular/core';
 import { Product } from '../../shared/models/Product';
-
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  private basePath = '/images';
-  file: File;
-  url = '';
+  imagesFromDB: any[];
 
-  constructor() { }
+  
 
-  getAll():Product[]{
-    return[
-      {
-        productName: 'Choclate Brownies', 
-        desc: 'Delicious choclate brownies with a melted center',
-        stars: 4.5,
-        imageUrl: '/assets/image/products/cake-1.jpg',
-        
-      },
-      {
-        productName: 'Choclate Cake',
-        desc: 'Delicious choclate cake with a sweet moist center',
-        stars: 4,
-        imageUrl: '/assets/image/products/cake-2.jpg',
-        
-      },
-      {
-        productName: 'Cupcakes',
-        desc: 'Cupcakes of all sorts of colours and flavours',
-        stars: 3.5,
-        imageUrl: '/assets/image/products/cake-3.jpg',
-        
-      },
-      {
-        productName: 'Strawberry Cake',
-        desc: 'strawberry cheescake with a lime topping',
-        stars: 3,
-        imageUrl: '/assets/image/products/cake-4.jpg',
-       
-      },
-                 
-    ]
-  }
+
+  
+
+  
+
 }
--- src/app/shared/models/Product.ts
+++ src/app/shared/models/Product.ts
@@ -4,5 +4,9 @@ export class Product{
     stars: number =0;
     imageUrl!:string;
 
-    
+    constructor(name ='', description = '',  imageUrl = '') {
+        this.productName = name;
+        this.desc = description;
+        this.imageUrl = imageUrl;
+}
 }
