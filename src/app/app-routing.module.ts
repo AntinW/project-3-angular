d2d33a37d629fba10d6fead0628bcc46fee2d594
RLX-Wilkinson <219071330@mycput.ac.za>
2021-09-28 11:27:36 +0200

    Added Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { GalleryPageComponent } from './gallery-page/gallery-page.component';
 import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [
   { path:'', component: HomepageComponent},
   {path: 'gallery-page',component: GalleryPageComponent},
   {path: 'search/:searchTerm', component: GalleryPageComponent},
-  {path: 'menu',component: MenuComponent}
+  {path: 'menu',component: MenuComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
